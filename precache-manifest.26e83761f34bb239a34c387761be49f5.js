6b11c024df16619fecd6fb3cf78a6d7264dbe6e9
gapyoqq <gapyoqq@gmail.com>
2021-02-04 13:04:17 +0300

    Updates

---
+++ precache-manifest.26e83761f34bb239a34c387761be49f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335e69c4e681ca601faf36ce40dc24b6",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "bb5737e570265cc86118",
+    "url": "/ToDoList/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "31f1cd2e4dd5e1264dca",
+    "url": "/ToDoList/static/js/2.d67fdfd0.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/ToDoList/static/js/2.d67fdfd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb5737e570265cc86118",
+    "url": "/ToDoList/static/js/main.fa6e1c14.chunk.js"
+  },
+  {
+    "revision": "448f1ac8991118963dc3",
+    "url": "/ToDoList/static/js/runtime-main.97949b04.js"
+  }
+]);
